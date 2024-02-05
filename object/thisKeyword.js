4bb166228a48292414ddc2e658c05a6206526bfa
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2024-02-05 21:19:03 +0600

    this keyword

---
+++ object/thisKeyword.js
@@ -0,0 +1,19 @@
+const car = {
+    name:"Toyota Premio",
+    model: 2018,
+    weight: "1000kg",
+    color: "Purple",
+    start: function(){
+        this.drive();
+        console.log("Car has started");
+    },
+    drive: function(){
+        console.log("Car is driving");
+    },
+};
+console.log(car.name);
+console.log(car.model);
+console.log(car.weight);
+console.log(car.color);
+car.start();
+// car.drive();
